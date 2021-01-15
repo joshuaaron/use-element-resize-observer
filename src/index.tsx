1ac6e27f492a8610662e8f805fda2cf03f7cd145
Josh Reynolds <josh@askottentertainment.com>
2021-01-14 21:45:07 -0800

    docs: add comments throughout index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,42 @@
 import { RefCallback, useCallback, useEffect, useRef, useState } from 'react';
 import { useIsomorphicLayoutEffect } from './helpers';
 
+/**
+ * Utility type defining the width and the height
+ */
 export type ObservedDimensions = {
     width: number | undefined;
     height: number | undefined;
 };
 
+/**
+ * Optional hook parameters.
+ */
 type ElementResizeObserverOptions = {
+    /**
+     * Callback that will receive the observed elements newly reported width and height
+     */
     onResize?: (dimensions: ObservedDimensions) => void;
 };
 
+/**
+ * Result returned from the hook.
+ */
 type ElementResizeObserverResult<T> = {
+    /**
+     * Callback ref created with `useCallback` that receives the DOM node of the element to be observed.
+     */
     assignRef: RefCallback<T>;
 } & ObservedDimensions;
 
+/**
+ * useElementResizeObserver
+ *
+ * React hook implementation that observes an element and reports on its changes to its width and height.
+ *
+ * @param {(dimensions: ObservedDimensions) => void} onResize Callback that receives the observed elements newly reported width and height
+ * @returns The callbackRef to be assigned to an element, and the observed elements width and height.
+ */
 export function useElementResizeObserver<T extends HTMLElement>({
     onResize,
 }: ElementResizeObserverOptions = {}): ElementResizeObserverResult<T> {
@@ -22,14 +45,17 @@ export function useElementResizeObserver<T extends HTMLElement>({
         height: undefined,
     });
 
+    // Keep track of previous dimensions and only update state or call onResize if the new values differ.
     const prevDimensions = useRef<ObservedDimensions>({
         width: undefined,
         height: undefined,
     });
 
+    // Store the observer instance, and the element forwarded via assignRef.
     const observerInstanceRef = useRef<ResizeObserver | null>(null);
     const callbackRef = useRef<T | null>();
 
+    // Store the onResize callback in a ref to ensure inline functions don't get recreated each time
     const onResizeRef = useRef<ElementResizeObserverOptions['onResize'] | undefined>(undefined);
     onResizeRef.current = onResize;
 
@@ -46,6 +72,7 @@ export function useElementResizeObserver<T extends HTMLElement>({
             return;
         }
 
+        // The function called whenever an observed resize occurs.
         const resizeCallback = ([entry]: readonly ResizeObserverEntry[]) => {
             let width;
             let height;
